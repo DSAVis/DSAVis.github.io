b7af88f1ae0677b98d4ffeaafc31305f18cd9403
Yash1632 <yash160203@gmail.com>
2022-10-17 01:50:22 +0530

    Base Working code for Sorting Algos

---
+++ src/homeComponents/cardDetails.js
@@ -0,0 +1,19 @@
+import sort from "./sort.png";
+export function getDetails() {
+  return [
+    {
+      id: 3,
+      title: "Sorting Algorithm",
+      description: "Compare different sorting algorithms",
+      route: "/sort",
+      img: sort,
+    },
+    {
+      id: 4,
+      title: "Recursive Sorting",
+      description: "Compare different recursive sorting algorithms",
+      route: "/recursivesort",
+      img: sort,
+    },
+  ];
+}
